b8c80bdf98ca8d43822560212f6eeaa739d18c8e
Marc-Eid <marc.mn.eid@gmail.com>
2022-02-03 18:35:49 -0500

    install primeflex and primeicons. #33

--- client/src/app/seller/seller.module.ts
+++ client/src/app/seller/seller.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SellerRoutingModule } from './seller-routing.module';
+import { OrdersComponent } from './orders/orders.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { ProductsComponent } from './products/products.component';
+import {AccordionModule} from 'primeng/accordion';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    OrdersComponent,
+    MyAccountComponent,
+    ProductsComponent
+  ],
   imports: [
     CommonModule,
-    SellerRoutingModule
+    SellerRoutingModule,
+    AccordionModule
   ]
 })
 export class SellerModule { }
