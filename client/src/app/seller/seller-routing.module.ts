6a03432e867a1c632f91f2c945d55ce4ee58bbda
Marc-Eid <marc.mn.eid@gmail.com>
2022-02-06 14:59:38 -0500

    fixed the link css properties and added the routing

--- client/src/app/seller/seller-routing.module.ts
+++ client/src/app/seller/seller-routing.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { OrdersComponent } from './orders/orders.component';
+import { ProductsComponent } from './products/products.component';
 import { SellerComponent } from './seller.component';
 
-const routes: Routes = [{
-  path: '', component: SellerComponent,
-    children: []
-  }
+const routes: Routes = [
+  {
+    path: '',
+    component: SellerComponent,
+    children: [
+      {
+        path: 'orders',
+        component: OrdersComponent,
+      },
+      {
+        path: 'my-account',
+        component: MyAccountComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      }
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class SellerRoutingModule { }
+export class SellerRoutingModule {}
