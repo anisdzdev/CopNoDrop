1b8c15611e139c8dc4bf042d1bca3692d9d119d1
Amin Boulemkahel <aminboulemkahel@gmail.com>
2022-03-14 22:04:08 -0400

    Implement user testing
    - implementation of all tests for GET, POST login, POST signup
    - fixing edit function error

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -3,9 +3,9 @@ const app = require("../app");
 
 describe("GET /users/:id", () => {
     test("The user id should be found, with the exact information and return success", async () => {
-        const response = await request(app).get('/users/6220e5214aa8c162396fe6fc');
+        const response = await request(app).get(`/users/622ff36674d32ed3752bc5d7`);
         expect(response.body).toEqual({
-            "_id": "6220e5214aa8c162396fe6fc",
+            "_id": "622ff36674d32ed3752bc5d7",
             "addresses": [],
             "avatar": "default.jpg",
             "email": "amin@test.com",
@@ -107,7 +107,7 @@ describe("POST /users/login", () => {
 
 describe("PUT /users/:id", () => {
     test("The user id should be found, change information and return success", async () => {
-        const updatedUser = await request(app).put('/users/6220e5214aa8c162396fe6fc').set({'x-auth-token':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjIwZTUyMTRhYThjMTYyMzk2ZmU2ZmMiLCJmaXJzdE5hbWUiOiJBbWluIiwibGFzdE5hbWUiOiJCb3VsZW1rYWhlbCIsImF2YXRhciI6ImRlZmF1bHQuanBnIiwiaXNTZWxsZXIiOmZhbHNlLCJlbWFpbCI6ImFtaW5AdGVzdC5jb20iLCJpYXQiOjE2NDcyOTM0MjR9.7z55iJClrUWe8DYhvhlQhkj3BhzsiwYtjetcDxJ1d2c"}).send(
+        const updatedUser = await request(app).put('/users/622ff36674d32ed3752bc5d7').set({'x-auth-token':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjIwZTUyMTRhYThjMTYyMzk2ZmU2ZmMiLCJmaXJzdE5hbWUiOiJBbWluIiwibGFzdE5hbWUiOiJCb3VsZW1rYWhlbCIsImF2YXRhciI6ImRlZmF1bHQuanBnIiwiaXNTZWxsZXIiOmZhbHNlLCJlbWFpbCI6ImFtaW5AdGVzdC5jb20iLCJpYXQiOjE2NDcyOTM0MjR9.7z55iJClrUWe8DYhvhlQhkj3BhzsiwYtjetcDxJ1d2c"}).send(
             {
                 "firstName": "Updated",
                 "lastName": "Boulemkahel",
