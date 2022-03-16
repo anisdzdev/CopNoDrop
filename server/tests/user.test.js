14302f2017e615804b7a26f795a82e28ee1bd18e
Amin Boulemkahel <aminboulemkahel@gmail.com>
2022-03-16 10:14:07 -0400

    Implement units tests
    - tests implementation for email, password, firstName, lastName and avatar

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -19,7 +19,7 @@ describe("EMAIL", () => {
 });
 
 describe("EMAIL", () => {
-    test("A not well formed email should return false", async () => {
+    test("A not well formed email should not match", async () => {
         const email = 'amintest.com';
         expect(email).not.toMatch(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     });
