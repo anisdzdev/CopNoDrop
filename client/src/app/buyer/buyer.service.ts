0d4568dde6e6f1d9c32cd60897e5feb284dec879
rogersi9 <69977233+rogersi9@users.noreply.github.com>
2022-03-17 21:16:15 -0400

    buyer can view orders and cancel unshipped #96 #95

--- client/src/app/buyer/buyer.service.ts
+++ client/src/app/buyer/buyer.service.ts
@@ -2,28 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Product } from 'libs/products/model/products';
 
 @Injectable({
   providedIn: 'root',
 })
 export class buyerService {
-  apiUrlProducts = environment.apiUrL + 'products';
+  apiUrl = environment.apiUrL;
 
   setHeaders(token: string) {}
   constructor(private http: HttpClient) {}
 
-  getProducts(): Observable<Product[]> {
-  
-
-    return this.http.get<Product[]>(this.apiUrlProducts);
-  }
-
-  getProduct(productId: string): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrlProducts}/${productId}`);
-  }
-
-  createProduct(productData: FormData, token: string): Observable<Product> {
+  getOrders(token: string){
     let requestOptions: Object = {
       headers: new HttpHeaders().append(
         'x-auth-token',
@@ -31,10 +20,10 @@ export class buyerService {
       ),
       responseType: 'text',
     };
-    return this.http.post<Product>(this.apiUrlProducts, productData, requestOptions);
+    return this.http.get(this.apiUrl+"orders"+"?sellerMode=false", requestOptions);
   }
 
-  updateProduct(productData: FormData, productid: string, token: string): Observable<Product> {
+  cancelOrder(order, token: string): Observable<any> {
     let requestOptions: Object = {
       headers: new HttpHeaders().append(
         'x-auth-token',
@@ -42,18 +31,8 @@ export class buyerService {
       ),
       responseType: 'text',
     };
-    
-    return this.http.put<Product>(`${this.apiUrlProducts}/${productid}`, productData, requestOptions);
-  }
 
-  deleteProduct(productId: string, token: string): Observable<any> {
-    let requestOptions: Object = {
-      headers: new HttpHeaders().append(
-        'x-auth-token',
-        token
-      ),
-      responseType: 'text',
-    };
-    return this.http.delete<any>(`${this.apiUrlProducts}/${productId}`, requestOptions);
-  }
+    return this.http.put(this.apiUrl+"orders/cancel/"+order._id, requestOptions);
+}
+
 }
--- client/src/app/buyer/orders/orders.component.ts
+++ client/src/app/buyer/orders/orders.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService, User } from 'src/app/auth/auth.service';
+import { buyerService } from '../buyer.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,36 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.scss'],
 })
 export class OrdersComponent implements OnInit {
+  user: User;
+  subscription: Subscription[] = [];
+  isLoggedIn: boolean;
   orders;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private buyerService: buyerService
+  ) {}
 
-  constructor() {
-  this.orders = [
-    {
-      id: "123456",
-      date: "Jan 24, 2020",
-      time: "20:04",
-      total: 150.50,
-      products: [
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-        {img: "https://d145dj1pf6foch.cloudfront.net/catalog/product/cache/1c3fb676c17af207704f6c7671f9267d/5/4/549521-v001.jpg"},
-      ]
-    }
-
-  ]
+  ngOnDestroy(): void {
+    this.subscription.map((sub) => sub.unsubscribe());
   }
 
   ngOnInit(): void {
+    this.authService.getUserFromStorage();
+    const sub = this.authService.isloginSubject.subscribe(
+      (value) => (this.isLoggedIn = value)
+    );
+    this.subscription.push(sub);
 
+    if (this.isLoggedIn) {
+      this.user = this.authService.getUserFromStorage();
+    } else {
+      this.router.navigateByUrl('');
+    }
+
+    this._getOrders();
+  }
+
+  private _getOrders() {
+    this.buyerService.getOrders(this.user.token).subscribe((orders: any) => {
+      this.orders = JSON.parse(orders);
+    });
+  }
+
+  cancelOrder(order) {
+    order.state = 'delivered';
+    this.buyerService.cancelOrder(order, this.user.token).subscribe();
   }
 }
